f32591e894baba533aa6e83726cf99a8206de2c0
ITmbuyu <41137033+ITmbuyu@users.noreply.github.com>
2023-01-14 18:37:11 +0200

    initial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+
+namespace DatingApp.Controllers
+{
+
+    [ApiController]
+    [Route("api/controller")]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+        //this is a constructor
+        public UsersController(DataContext context)
+        {
+            _context = context;
+             
+        }
+
+        [HttpGet]
+        public async  Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            //var user = _context.Users.Find(id);
+
+            //return user;
+
+            //or 
+
+            return await _context.Users.FindAsync(id);
+        }
+
+
+    }
+}
